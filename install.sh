04523bd36fceeb76a3103bb04f13ab95274332db
Ottony Costa <ottony93@gmail.com>
2013-12-16 13:32:01 -0300

    Show messages :metal:

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 cd ~; mv .vimrc .vimrc.b; mv .tmux.conf .tmux.conf.b; echo "removed before configurations. Files renamed to .vimrc.b and .tmux.conf.b";
+echo "Cloning https://github.com/ottony/dotfiles.git into ~/ ...";
 git clone https://github.com/ottony/dotfiles.git ~/;
+echo "Cloning vundle https://github.com/gmarik/vundle.git into ~/.vim/bundle/vundle ...";
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle;
+echo "Installing plugins ...";
 vim +BundleInstall +qall;
+echo "Everything is up to date ;)";
