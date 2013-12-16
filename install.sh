fa6391a3879859d2574d74302b0ecbc1016f35b9
Ottony Costa <ottony93@gmail.com>
2013-12-16 13:34:10 -0300

    FIX - destination of cloning

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 cd ~; mv .vimrc .vimrc.b; mv .tmux.conf .tmux.conf.b; echo "removed before configurations. Files renamed to .vimrc.b and .tmux.conf.b";
-echo "Cloning https://github.com/ottony/dotfiles.git into ~/ ...";
-git clone https://github.com/ottony/dotfiles.git ~/;
+echo "Cloning https://github.com/ottony/dotfiles.git into ~/. ...";
+git clone https://github.com/ottony/dotfiles.git ~/.;
 echo "Cloning vundle https://github.com/gmarik/vundle.git into ~/.vim/bundle/vundle ...";
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle;
 echo "Installing plugins ...";
