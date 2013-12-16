ffc4be0d6d4cfd0e5fe73ec77eb5e14d394e4b95
Ottony Costa <ottony93@gmail.com>
2013-12-16 14:01:33 -0300

    Turn pretty messages :neckbeard:

--- install.sh
+++ install.sh
@@ -1,16 +1,25 @@
 cd ~; mv .vimrc .vimrc.b; mv .tmux.conf .tmux.conf.b; 
-echo "removed before configurations. Files renamed to .vimrc.b and .tmux.conf.b";
+echo "  ";
+echo "\033[1;34mremoved before configurations. Files renamed to .vimrc.b and .tmux.conf.b\033[0m";
 
-echo "Get .vimrc file ...";
+echo "  ";
+echo "\033[1;34mGet .vimrc file ...\033[0m";
+echo " ";
 curl https://raw.github.com/ottony/dotfiles/master/.vimrc >> ~/.vimrc;
 
-echo "Get .tmux.conf file ...";
+echo "  ";
+echo "\033[1;34mGet .tmux.conf file ...\033[0m";
+echo " ";
 curl https://raw.github.com/ottony/dotfiles/master/.tmux.conf >> ~/.tmux.conf;
 
-echo "Cloning vundle https://github.com/gmarik/vundle.git into ~/.vim/bundle/vundle ...";
+echo "  ";
+echo "\033[1;36mCloning vundle https://github.com/gmarik/vundle.git into ~/.vim/bundle/vundle ...\033[0m";
+echo " ";
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle;
 
-echo "Installing plugins ...";
+echo "  ";
+echo "\033[1;33mInstalling plugins ...\033[0m";
 vim +BundleInstall +qall;
 
-echo "Everything is up to date ;)";
+echo "  ";
+echo "\033[1;32mEverything is up to date ;)\033[0m";
